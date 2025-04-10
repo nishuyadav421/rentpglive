5e2ad809cd0c5ab5e181de0d423f873953319296
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-04-10 19:31:34 +0530

    login system

--- routes/user.py
+++ routes/user.py
@@ -1,8 +1,32 @@
-from flask import jsonify
+from flask import jsonify,request
 
 def init_user_routes(app):
-    @app.route('/login')
-    def login():
+    @app.route('/api/v1/user/login', methods=['POST'])
+    def user_login():
+        email = request.form.get('email', '')
+        password = request.form.get('password', '')
+
+        # Example response
         return jsonify({
-            "massage":"I am here to help on the backend problem "
+            'email': email,
+            'password': password,
+            'message': 'Login route hit successfully!'
+        })
+
+
+    @app.route('/api/v1/user/register', methods=['POST'])
+    def user_register():
+        email = request.form.get('email', '')
+        password = request.form.get('password', '')
+        mobile=request.form.get('mobile', '')
+        name=request.form.get('name', '')
+        address=request.form.get('address', '')
+        # Example response
+        return jsonify({
+            'email': email,
+            'password': password,
+            'mobile': mobile,
+            'name': name,
+            'address': address,
+            'message': 'Register route hit successfully!'
         })
