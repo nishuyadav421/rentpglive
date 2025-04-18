8a78f8003747e15e1114ec5795edbdf9b58039b8
MeLiveAkshay <akshaykumarprajapati@hotmail.com>
2025-04-18 21:51:07 +0530

    done menu and add some extra page on the website

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from routes.main import init_main_routes
 from routes.user import init_user_routes
+from routes.page import init_page_routes
 
 app = Flask(__name__)
 init_main_routes(app)
 init_user_routes(app)
+init_page_routes(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,17 @@
 const inputSearch = document.querySelector('.inputSearch');
+const loginBtn = document.getElementById('loginBtn');
+const registerBtn = document.getElementById('registerBtn');
 
 inputSearch.addEventListener('click', () => {
     inputSearch.style.backgroundColor = 'red';
 });
+
+loginBtn.addEventListener('click', () => {
+    alert('Login route hit successfully!')
+   
+});
+
+registerBtn.addEventListener('click', () => {
+    alert('Register route hit successfully!')
+});
+
