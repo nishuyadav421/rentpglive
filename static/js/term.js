9c8747bab5e23aa1da0fd47cbbd7f1da040d6f08
Nitu <168166540+tinumehta32@users.noreply.github.com>
2025-04-27 18:01:19 +0530

    my admin

---
+++ static/js/term.js
@@ -0,0 +1,5 @@
+document.getElementById('acceptBtn').addEventListener('click', function() {
+    alert('Thank you for accepting the Terms and Conditions!');
+    // Redirect to homepage or another page if needed
+    // window.location.href = 'homepage.html';
+});
